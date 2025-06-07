864e83aaa016e11fd4c66e1e58a13fe2b460498a
Mayank Jangid <226301131@gkv.ac.in>
2025-06-07 12:12:29 +0530

    Initial Commit

---
+++ Movie assgn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movie assgn
+//
+//  Created by Mayank Jangid on 6/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movie assgn/Movie_assgnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_assgnApp.swift
+//  Movie assgn
+//
+//  Created by Mayank Jangid on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Movie_assgnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
